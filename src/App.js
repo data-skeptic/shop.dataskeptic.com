ea3bba8259db645610a260835168fee9b8d429d5
Kyle Polich <kyle@dataskeptic.com>
2020-02-11 14:40:48 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Data Skeptic: Store</h1>
+        <p>Please take a look around.</p>
+        <div>
+          Put all items here
+        </div>
+    </div>
+  );
+}
+
+export default App;
